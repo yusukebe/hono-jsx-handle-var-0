33eaaa674a4ba76b127a21cfb15818108942bed5
Yusuke Wada <yusuke@kamawada.com>
2024-01-07 00:04:57 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+const FC_VAR = () => <span>{0}</span>
+const FC_NUM = () => <span>0</span>
+
+console.log(<FC_VAR />.toString()) // <span children="0"></span>
+console.log(<FC_NUM />.toString()) // <span>0</span>
